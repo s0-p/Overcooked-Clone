213ca6aa07f044397d84420274d9d8ece348c799
s0 <b00v0429@gmail.com>
2024-01-10 13:44:50 +0900

    fix : table detection

--- Assets/Scripts/PlayerCtrl.cs
+++ Assets/Scripts/PlayerCtrl.cs
@@ -9,6 +9,9 @@ public class PlayerCtrl : MonoBehaviour
     Vector3 _moveDir;
     //-----------------------------------
     [SerializeField]
+    float _throwPower = 30;
+    //-----------------------------------
+    [SerializeField]
     Transform _pickupTransform;
     Transform _detectedTableTransfom;
     //-----------------------------------
@@ -27,54 +30,78 @@ public class PlayerCtrl : MonoBehaviour
             transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(_moveDir), Time.deltaTime * _rotSpeed);
         }
     }
-    void OnTriggerEnter(Collider other)
+    void OnTriggerStay(Collider other)
     {
         if (other.CompareTag("Table"))
         {
+            if (_detectedTableTransfom != null)
+                _detectedTableTransfom.GetComponent<Table>().Exit();
+
             _detectedTableTransfom = other.transform;
+            _detectedTableTransfom.GetComponent<Table>().Enter();
         }
     }
     void OnTriggerExit(Collider other)
     {
         if (other.CompareTag("Table"))
         {
-            _detectedTableTransfom = null;
+            if (_detectedTableTransfom != null)
+            {
+                _detectedTableTransfom.GetComponent<Table>().Exit();
+                _detectedTableTransfom = null;
+            }
         }
     }
     private void Update()
     {
-        //Space 입력이 있고 테이블을 인식했을 경우
-        if (Input.GetKeyDown(KeyCode.Space) && _detectedTableTransfom != null)
+        //leftctrl 입력이 있고 손에 물건이 있으면 던지기
+        if (Input.GetKeyDown(KeyCode.LeftControl) && _pickupTransform.childCount > 0)
+            Throw();
+
+        //Space 입력
+        else if (Input.GetKeyDown(KeyCode.Space))
         {
-            //손에 물건이 있고 테이블이 비어 있으면 물건 놓기
-            if (_pickupTransform.childCount > 0)
+            //테이블을 인식했을 경우
+            if (_detectedTableTransfom != null)
             {
-                Transform foodTransform = _pickupTransform.GetChild(0);
-                if(foodTransform != null) 
+                //손에 물건이 있고 테이블이 비어 있으면 물건 놓기
+                if (_pickupTransform.childCount > 0)
                 {
-                    foodTransform.parent = _detectedTableTransfom;
-                    foodTransform.position = _detectedTableTransfom.position + Vector3.up;
+                    Transform foodTransform = _pickupTransform.GetChild(0);
+                    if (foodTransform != null)
+                    {
+                        foodTransform.parent = _detectedTableTransfom;
+                        foodTransform.position = _detectedTableTransfom.position + Vector3.up;
+                    }
+                    else
+                    {
+                        foodTransform = _detectedTableTransfom.GetChild(0);
+                        if (foodTransform != null)
+                        {
+                            foodTransform.parent = _pickupTransform;
+                            foodTransform.position = _pickupTransform.position;
+                        }
+                    }
                 }
+                //테이블에 물건이 있고 손이 비어있으면 물건 들기
                 else
                 {
-                    foodTransform = _detectedTableTransfom.GetChild(0);
-                    if(foodTransform != null ) 
+                    if (_pickupTransform.childCount <= 0)
                     {
-                        foodTransform.parent = _pickupTransform;
-                        foodTransform.position = _pickupTransform.position;
+                        Transform itemTransform = _detectedTableTransfom.GetChild(0);
+                        itemTransform.position = _pickupTransform.position;
+                        itemTransform.parent = _pickupTransform;
                     }
                 }
             }
-            //테이블에 물건이 있고 손이 비어있으면 물건 들기
-            else
-            {
-                if (_pickupTransform.childCount <= 0)
-                {
-                    Transform itemTransform = _detectedTableTransfom.GetChild(0);
-                    itemTransform.position = _pickupTransform.position;
-                    itemTransform.parent = _pickupTransform;
-                }
-            }
         }
+    }
+    void Throw()
+    {
+        Rigidbody itemRigidbody = _pickupTransform.GetComponentInChildren<Rigidbody>();
+        itemRigidbody.useGravity = true;
+        itemRigidbody.isKinematic = false;
+        itemRigidbody.transform.parent = null;
+        itemRigidbody.AddForce(transform.forward * _throwPower);
     }
 }
--- Assets/Scripts/Table.cs
+++ Assets/Scripts/Table.cs
@@ -13,18 +13,7 @@ public class Table : MonoBehaviour
         _renderer = GetComponent<Renderer>();
         _originShader = _renderer.material.shader;
     }
-    void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            _renderer.material.shader = _detectedShader;
-        }
-    }
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            _renderer.material.shader = _originShader;
-        }
-    }
+    //-----------------------------
+    public void Enter() { _renderer.material.shader = _detectedShader; }
+    public void Exit() { _renderer.material.shader = _originShader; }
 }
