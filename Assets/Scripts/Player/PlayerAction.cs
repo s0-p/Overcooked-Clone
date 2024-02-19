cf274b6173533dc70eef7980d3e4df4b05d4879a
s0 <b00v0429@gmail.com>
2024-02-19 15:29:19 +0900

    rename : PlayerDetect -> PlayerAction

--- Assets/Scripts/Player/PlayerAction.cs
+++ Assets/Scripts/Player/PlayerAction.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerDetect : MonoBehaviour
+public class PlayerAction : MonoBehaviour
 {
     [SerializeField]
     float _throwPower = 30;
